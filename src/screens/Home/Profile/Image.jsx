04f6be289f841594057272880fa40df66e1659e8
Ruihua Sui <rsui2@illinois.edu>
2019-12-20 03:34:38 -0600

    css update

--- src/screens/Home/Profile/Image.jsx
+++ src/screens/Home/Profile/Image.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { profilePhoto } from '../../../assets'
 
 export default function Image({
-
+  dark=100
 }) {
+  console.error(dark+'%')
   return (
-    <div className="srh-profile-img">
+    <div className="srh-profile-img" style={{ filter: `brightness(${dark}%)`}}>
       <img 
         src={profilePhoto}
+        data-darkness={dark+'%'}
       />
     </div>
   )
--- src/screens/Home/Profile/index.js
+++ src/screens/Home/Profile/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './index.css'
 
 import Image from './Image'
@@ -6,9 +6,20 @@ import Links from './Links'
 import { Card } from '../../../components/Card'
 
 export default function Profile({}) {
+  const [dark, setDark] = useState(100)
+  
+  useEffect(() => {
+    document.addEventListener('scroll', e => {
+      if (window.innerWidth <= 900 && window.scrollY * 0.4 <= 100) {
+        let d = parseInt(100 - window.scrollY * 0.4)
+        setDark(d < 20 ? 0 : d)
+      }
+    })
+  }, [])
+
   return (
     <div className="srh-profile">
-      <Image />
+      <Image dark={dark} />
       <div className="profile-info">
         <h1 className="srh-name">R<span>UIHUA</span> S<span>UI</span></h1>
         <p className="srh-aboutme">
